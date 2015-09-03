eac5ed83857c629bfbea51b02b822fc758adc17e
François Févotte <fevotte@gmail.com>
2015-09-03 22:42:28 +0200

    Use `hydra` to define the DWIM command
    - the DWIM command is now called NAME-dwim
    - it is left to the user to bind it (the :key optional parameter
      disappeared)
    - it offers a menu (implemented using `hydra`) from which different
      compilation-related commands can be run

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ commands to easily automate running multiple shell commands.
 
 ## Installation
 
-From `git`:
+First install the required dependency:
+[`hydra`](http://github.com/abo-abo/hydra). It is available from various ELPA
+repositories. Then:
 
 1. get this repository:
 
@@ -15,7 +17,8 @@ From `git`:
    $ git clone https://github.com/ffevotte/multi-compile
    ```
 
-2. add this to your init file:
+2. instruct Emacs where to find the package by adding a snippet like this in
+   your init file:
 
    ```lisp
    (add-to-list 'load-path "/path/to/multi-compile")
@@ -29,45 +32,26 @@ From `git`:
 Putting the following snippet in your init file:
 
 ```lisp
-(multi-compile "check"
-  :key     (kbd "<f5>")  ;; optional
+(multi-compile check
   :command "make check"  ;; optional
   )
 ```
 
 defines the following commands:
 
-- <kbd>M-x</kbd>`check`: similar to `compile`, except that it has its own shell command and execution directory.
+- <kbd>M-x</kbd>`check`: similar to `compile`, except that it has its own shell
+  command and execution directory. Compilation results are put in a buffer named `*check*`.
 
-- <kbd>M-x</kbd>`recheck`: similar to `recompile`, except that it shares the compilation command with `check`.
+- <kbd>M-x</kbd>`recheck`: similar to `recompile`, except that it reuses the
+  last compilation parameters from the buffer `*check*`.
+
+- <kbd>M-x</kbd>`check-dwim`: when called without argument, call `recheck`. With
+  a prefix argument, offer a choice between several compilation-related
+  commands. This command is returned by `multi-compile`, so that you can bind it
+  directly if you like, as in the examples below.
 
 
 The following optional parameters can be provided:
-
-- `:key` creates a new emacs command (<kbd>M-x</kbd>`check-or-recheck`), bound to the provided key (<kbd>F5</kbd> in the example). This command executes either one of the previous two commands depending on the prefix argument:
-
-  <table>
-  <tr>
-    <th>Key</th>
-    <th>Equivalent&nbsp;command</th>
-    <th>Effect</th>
-  </tr>
-  <tr>
-    <td><kbd>F5</kbd>&nbsp;</td>
-    <td><kbd>M-x</kbd>&nbsp;<tt>recheck</tt></td>
-    <td>Re-run last compilation command (define it the first time)</td>
-  </tr>
-  <tr>
-    <td><kbd>C-u</kbd>&nbsp;<kbd>F5</kbd></td>
-    <td><kbd>C-u</kbd>&nbsp;<kbd>M-x</kbd>&nbsp;<tt>recheck</tt></td>
-    <td>Modify the compilation command (but not the directory)</td>
-  </tr>
-  <tr>
-    <td><kbd>C-u</kbd>&nbsp;<kbd>C-u</kbd>&nbsp;<kbd>F5</kbd></td>
-    <td><kbd>C-u</kbd>&nbsp;<kbd>M-x</kbd>&nbsp;<tt>check</tt></td>
-    <td>Run the compilation command in an interactive buffer</td>
-  </tr>
-  </table>
 
 - `:command` defines a default value for the shell command associated to the new compilation command.
 
@@ -77,8 +61,8 @@ The following optional parameters can be provided:
 A C++ developer might setup the following commands to automate the development process:
 
 ```lisp
-(multi-compile "build"     :key (kbd "<f5>"))
-(multi-compile "check"     :key (kbd "<f6>")  :command "make check")
-(multi-compile "install"   :key (kbd "<f7>")  :command "make install")
-(multi-compile "build-doc" :key (kbd "<f8>")  :command "make doc")
+(global-set-key (kbd "<f5>") (multi-compile build))
+(global-set-key (kbd "<f6>") (multi-compile check      :command "make check"))
+(global-set-key (kbd "<f7>") (multi-compile install    :command "make install"))
+(global-set-key (kbd "<f8>") (multi-compile build-doc  :command "make doc"))
 ```
--- multi-compile.el
+++ multi-compile.el
@@ -1,10 +1,11 @@
 ;;; multi-compile.el --- Define multiple compilation commands
 
-;; Copyright (C) 2012 François Févotte
+;; Copyright (C) 2012-2015 François Févotte
 
 ;; Author: François Févotte <fevotte@gmail.com>
 ;; Created: 10 Mar 2014
-;; Version: 1.0
+;; Package-Version: 1.1
+;; Package-Requires: ((hydra "0.13.0"))
 ;; Keywords: compilation
 ;; X-URL: https://github.com/ffevotte/multi-compile
 
@@ -31,13 +32,12 @@
 ;; commands.
 ;;
 ;; For example, the following snippet:
-;;    (multi-compile "build" :key (kbd "<f5>"))
-;;    (multi-compile "check" :key (kbd "<f6>")  :command "make check")
+;;    (multi-compile build)
+;;    (multi-compile check :command "make check")
 ;;
-;; would define new commands named `build' and `check', respectively bound to
-;; <f5> and <f6>. The first time you call each of these commands, you will be
-;; asked to provide the associated compilation command by editing a default
-;; value:
+;; would define new commands named `build' and `check'. The first time you call
+;; each of these commands, you will be asked to provide the associated
+;; compilation command by editing a default value:
 ;;
 ;;    build:  "make -k"    (inherited from `compile-command')
 ;;    check:  "make check" (setup in the `multi-compile' declaration)
@@ -54,6 +54,10 @@
 
 ;;; Code:
 
+
+(eval-and-compile
+  (require 'hydra))
+
 (defmacro multi-compile (name &rest args)
   "Define a new compilation command.
 
@@ -66,21 +70,24 @@ NAME (e.g M-x build)
 reNAME (e.g. M-x rebuild)
     similar to `recompile'
 
-Optional arguments:
+NAME-dwim (e.g. M-x build-dwim)
+    - without prefix argument, run `recompile'
+    - with a prefix argument, offer a choice between several
+      compilation-related commands
 
-:key Bind the given key to a new command which executes either
-     one of the preceding commands, depending on the prefix
-     argument. See the docstring for the key for more details.
+Optional arguments ARGS are composed of a plist with the following keys:
 
-:command Define a default shell command associated to the newly
-     created `compile' command."
+:command CMD
+    Define a default shell command associated to the newly
+    created `compile' command.
+"
   (let* ((key              (plist-get args :key))
          (default-command  (plist-get args :command))
-         (buffer-name      (concat "*" name "*"))
-         (compile-symbol   (intern name))
-         (recompile-symbol (intern (concat "re" name)))
-         (compile-or-recompile-symbol
-          (intern (format "%s-or-re%s" name name)))
+         (buffer-name      (format "*%s*" name))
+         (compile-symbol   name)
+         (recompile-symbol (intern (format "re%s" name)))
+         (dwim-symbol      (intern (format "%s-dwim" name)))
+         (hydra-symbol     (intern (format "%s-hydra" name)))
          (let-form `((compilation-buffer-name-function
                       (lambda (mode) "" ,buffer-name)))))
     (when (fboundp compile-symbol)
@@ -88,50 +95,82 @@ Optional arguments:
     (when (fboundp recompile-symbol)
       (warn "redefining command `re%s'" name))
     (when (not (null default-command))
-      (setq let-form (cons `(compile-command ,default-command) let-form)))
-    `(progn
+      (push `(compile-command ,default-command) let-form))
+    `(prog1
+         (defun ,dwim-symbol (argp)
+           ,(format
+             "This command behaves similary to `%s', with alternatives.
+
+When called without argument, call `%s'.
+
+Otherwise, when ARGP is non-nil, offer a choice between multiple
+compilation-related commands:
+
+- `%s' using the last compilation parameters;
+
+- run an interactive `%s' compilation (uses Comint with
+  `compilation-shell-minor-mode');
+
+- re-run the `%s' compilation command, resetting directory and
+  command;
+
+- `%s', changing the compilation command;
+
+- `%s', changing the compilation directory."
+             recompile-symbol recompile-symbol recompile-symbol
+             compile-symbol   compile-symbol
+             recompile-symbol recompile-symbol)
+           (interactive "P")
+           (if (null argp)
+               (call-interactively #',recompile-symbol)
+             (call-interactively (get ',dwim-symbol :alternate-hydra))))
+
        (defun ,compile-symbol ()
          ,(format
-           "This function behaves similarly to `compile', except it puts compilation
+           "This command behaves similarly to `compile', except it puts compilation
 results in the %s buffer." buffer-name)
          (interactive)
          (let ,let-form
-           (call-interactively 'compile)))
+           (call-interactively #'compile)))
 
        (defun ,recompile-symbol ()
          ,(format
-           "This function behaves similarly to `recompile', except it reuses the
+           "This command behaves similarly to `recompile', except it reuses the
 last compilation parameters from buffer %s." buffer-name)
          (interactive)
          (if (get-buffer ,buffer-name)
              (with-current-buffer ,buffer-name
                (let ,let-form
-                 (call-interactively 'recompile)))
-           (call-interactively ',compile-symbol)))
+                 (call-interactively #'recompile)))
+           (call-interactively #',compile-symbol)))
 
-       ,(when key
-          `(global-set-key ,key
-                           (defun ,compile-or-recompile-symbol (arg)
-                             ,(format "Run or re-run the compilation in buffer %s.
-The exact effect depends on the prefix argument:
-
-Key            Command              Effect
--------------- -------------------- -------------------------------
-%-6s         M-x %-12s     re-run last compilation command
-                                        (define it the first time)
-C-u %-6s     C-u M-x %-12s modify compilation command
-C-u C-u %-6s C-u M-x %-12s run compilation command in an
-                                        interactive buffer"
-                                      buffer-name
-                                      (key-description (eval key)) (symbol-name recompile-symbol)
-                                      (key-description (eval key)) (symbol-name recompile-symbol)
-                                      (key-description (eval key)) (symbol-name compile-symbol))
-                             (interactive "P")
-                             (cond ((equal arg '(16))
-                                    (let ((current-prefix-arg '(4)))
-                                      (call-interactively ',compile-symbol)))
-                                   (t
-                                    (call-interactively ',recompile-symbol)))))))))
+       (put ',dwim-symbol :alternate-hydra
+            (defhydra ,hydra-symbol (:exit t)
+              ,(format "
+%s:
+  _r_ecompile           recompile and change:
+  _i_nteractive           _c_ommand
+  _R_eset everything      _d_irectory
+" name)
+              ("r" (let ((current-prefix-arg nil))
+                     (call-interactively #',recompile-symbol))
+               nil)
+              ("i" (let ((current-prefix-arg '(4)))
+                     (call-interactively #',compile-symbol))
+               nil)
+              ("R" (let ((current-prefix-arg nil))
+                     (call-interactively #',compile-symbol))
+               nil)
+              ("c" (let ((current-prefix-arg '(4)))
+                     (call-interactively #',recompile-symbol))
+               nil)
+              ("d" (with-current-buffer (get-buffer ,buffer-name)
+                     (setq current-prefix-arg nil
+                           compilation-directory
+                           (read-directory-name "Compile in directory:"
+                                                compilation-directory))
+                     (call-interactively #',recompile-symbol))
+               nil))))))
 
 (provide 'multi-compile)
 ;;; multi-compile.el ends here
